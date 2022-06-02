cb01079a74dfcf06901c040508cf641fe05ed9d4
Alex Martins <brancomrt@gmail.com>
2022-06-02 00:45:03 +0000

    .

--- dependencias_zabbix_ec2.sh
+++ dependencias_zabbix_ec2.sh
@@ -7,7 +7,7 @@ sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable --now docker
 sudo yum install git -y
-#sudo mkdir -p /var/local/storage && sudo chmod 755 /var/local/storage
+sudo mkdir -p /var/local/storage && sudo chmod 775 /var/local/storage
 cd /tmp && git clone https://github.com/brancomrt/bootcamp-clc7.git && cd /tmp/bootcamp-clc7 && docker-compose up -d
 sudo hostnamectl set-hostname zabbix-server.localdomain
 sudo sed -i -e '$a\' -e 'preserve_hostname: true' /etc/cloud/cloud.cfg
