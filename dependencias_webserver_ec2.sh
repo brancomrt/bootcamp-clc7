dbf2961d373b2f2bb16257bc0b105b1b6057d53b
Alex Martins <brancomrt@gmail.com>
2022-06-03 00:29:44 +0000

    .

--- dependencias_webserver_ec2.sh
+++ dependencias_webserver_ec2.sh
@@ -5,9 +5,11 @@ sudo amazon-linux-extras install nginx1 -y
 sudo systemctl enable --now nginx
 sudo hostnamectl set-hostname webserver.localdomain
 sudo sed -i -e '$a\' -e 'preserve_hostname: true' /etc/cloud/cloud.cfg
-sudo yum install https://repo.zabbix.com/zabbix/5.4/rhel/7/x86_64/zabbix-release-5.4-1.el7.noarch.rpm
+sudo yum install https://repo.zabbix.com/zabbix/5.4/rhel/7/x86_64/zabbix-release-5.4-1.el7.noarch.rpm -y
 sudo yum install zabbix-agent2 -y
-net-snmp net-snmp-utils -y
-sudo sed -i -e '$a\' -e 'rocommunity atech' /etc/snmp/snmpd.conf
+sudo yum install net-snmp-utils net-snmp -y
+sudo sed -i -e '$a\' -e 'rocommunity bootcamp' /etc/snmp/snmpd.conf
 sudo systemctl enable --now snmpd
-sudo echo "Server=127.0.0.1,10.0.1.100" /etc/zabbix/zabbix_agent2.d/01-bootcamp
+sudo touch /etc/zabbix/zabbix_agent2.d/01-bootcamp && sudo chmod 775 /etc/zabbix/zabbix_agent2.d/01-bootcamp
+sudo sed -i -e '$a\' -e 'Server=127.0.0.1,10.0.1.100' /etc/zabbix/zabbix_agent2.d/01-bootcamp && sudo chmod 644 /etc/zabbix/zabbix_agent2.d/01-bootcamp
+sudo systemctl enable --now zabbix-agent2
