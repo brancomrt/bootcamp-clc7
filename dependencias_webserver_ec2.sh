1b9198bf846015be52814581ce6f97250e73f3f0
Alex Martins <brancomrt@gmail.com>
2022-06-03 14:06:42 +0000

    .

--- dependencias_webserver_ec2.sh
+++ dependencias_webserver_ec2.sh
@@ -11,5 +11,5 @@ sudo yum install net-snmp-utils net-snmp -y
 sudo sed -i -e '$a\' -e 'rocommunity bootcamp' /etc/snmp/snmpd.conf
 sudo systemctl enable --now snmpd
 sudo touch /etc/zabbix/zabbix_agent2.d/01-bootcamp && sudo chmod 775 /etc/zabbix/zabbix_agent2.d/01-bootcamp
-sudo sed -i -e '$a\' -e 'Server=127.0.0.1,10.0.1.100' /etc/zabbix/zabbix_agent2.d/01-bootcamp && sudo chmod 644 /etc/zabbix/zabbix_agent2.d/01-bootcamp
+sudo sed -i s/Server=127.0.0.1/Server=127.0.0.1,10.0.1.100' /etc/zabbix/zabbix_agent2.d/01-bootcamp && sudo chmod 644 /etc/zabbix/zabbix_agent2.d/01-bootcamp
 sudo systemctl enable --now zabbix-agent2
