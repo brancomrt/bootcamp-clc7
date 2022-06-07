34dc9c0c63c90240eafaad18872460c994d902c1
Alex Martins <brancomrt@gmail.com>
2022-06-07 23:08:28 +0000

    .

--- dependencias_zabbix_ec2.sh
+++ dependencias_zabbix_ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install yum-util git telnet -y
+sudo yum install yum-util git telnet net-snmp-utils net-snmp telnet -y
 sudo amazon-linux-extras install docker -y
 sudo usermod -aG docker ec2-user 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
