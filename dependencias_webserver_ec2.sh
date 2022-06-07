92f406a44e65e84cf4df35be73394fad4e51faf1
Alex Martins <brancomrt@gmail.com>
2022-06-07 21:47:02 +0000

    .

--- dependencias_webserver_ec2.sh
+++ dependencias_webserver_ec2.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install yum-utils -y
+sudo yum install yum-utils net-snmp-utils net-snmp telnet -y
+sudo yum install https://repo.zabbix.com/zabbix/5.4/rhel/7/x86_64/zabbix-release-5.4-1.el7.noarch.rpm -y
+sudo yum install zabbix-agent2 -y
 sudo amazon-linux-extras install nginx1 -y
 sudo systemctl enable --now nginx
 sudo hostnamectl set-hostname web-server.localdomain
 sudo sed -i -e '$a\' -e 'preserve_hostname: true' /etc/cloud/cloud.cfg
-sudo yum install https://repo.zabbix.com/zabbix/5.4/rhel/7/x86_64/zabbix-release-5.4-1.el7.noarch.rpm -y
-sudo yum install zabbix-agent2 -y
-sudo yum install net-snmp-utils net-snmp -y
 sudo sed -i -e '$a\' -e 'rocommunity bootcamp' /etc/snmp/snmpd.conf
 sudo systemctl enable --now snmpd
 sudo sed -i s/Server=127.0.0.1/Server=127.0.0.1,zabbix-server/g /etc/zabbix/zabbix_agent2.conf
--- dependencias_zabbix_ec2.sh
+++ dependencias_zabbix_ec2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install yum-utils -y
+sudo yum install yum-util git telnet -y
 sudo amazon-linux-extras install docker -y
 sudo usermod -aG docker ec2-user 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo systemctl enable --now docker
-sudo yum install git -y
 sudo mkdir -p /var/local/storage && sudo chmod 775 /var/local/storage
-cd /tmp && git clone https://github.com/brancomrt/bootcamp-clc7.git && cd /tmp/bootcamp-clc7 && docker-compose up -d
+sudo mkdir -p /var/local/storage/mysql_dump/var/lib/init && sudo chmod 775 /var/local/storage/mysql_dump/var/lib/init
+cd /tmp && git clone https://github.com/brancomrt/bootcamp-clc7.git && cd /tmp/bootcamp-clc7 && cp -a dump.sql /var/local/storage/mysql_dump/var/lib/init && docker-compose up -d
 sudo hostnamectl set-hostname zabbix-server.localdomain
 sudo sed -i -e '$a\' -e 'preserve_hostname: true' /etc/cloud/cloud.cfg
 cat >> /etc/hosts <<EOF
